2d7a42daa2a96af0cce0efc53ba40fb9183b72ec
y2gon2 <y2gon2@gmail.com>
2023-06-30 19:45:21 +0900

    revised: the fields data revised to i64 from BigUint

--- basic_bitcoin/src/secp256k1/field_element.rs
+++ basic_bitcoin/src/secp256k1/field_element.rs
@@ -1,78 +1,46 @@
 //! Finite Field 정의 및 해다 구현체의 연산 정의
 //! 
-//! 해당 struct field 는 추후 SHA256 Hash value 를 포함할 수 있도록 BigUint type 으로 설
-//! prime field 값은 책에 명시된 2^256 - 2^32 - 977 로 고정
+//! 해당 struct field type 은 i64 적용
 //! 
-//! BigUint type 을 기본으로 아래와 같이 Finite Field 연산 구현  
-//! 현재 구현된 연산    : ==, != , +, -, *
-//! 미구현된 연산       :/ , %
-//! 거듭 제곱의 경우 exponent - BitUint type 으로 구현
 
-use std::error::Error;
 use std::fmt::{Display};
-use std::ops::{Add, Sub, Mul};
-
-use num::{BigUint, Zero, One, FromPrimitive};
+use std::ops::{Add, Sub, Mul, Div};
 
 #[derive(Debug, Clone)]
 pub struct FieldElement {
-    num: BigUint,
-    prime: BigUint,
+    num: i64,
+    prime: i64,
 }
 
 
 impl FieldElement {
-    pub fn new(num: BigUint) -> Result<Self, Box<dyn Error>> {        
-        let secp256k1_prime = 
-            BigUint::from(2u64).pow(256) - BigUint::from(2u64).pow(32) - BigUint::from(977u64);
-        
-        if num >= secp256k1_prime {
-            let msg = format!("Num {} not in field range 0 to {}", num, secp256k1_prime - BigUint::from(1u64));
-            return Err(msg.into());
-        }
-        Ok(Self {
+    pub fn new(num: i64, prime: i64) -> Self { 
+        Self {
             num,
-            prime: secp256k1_prime,
-        })
+            prime,
+        }
     }
 
-    pub fn get_prime(&self) -> &BigUint {
-        &self.prime
+    pub fn get_prime(&self) -> i64 {
+        self.prime
     }
 
-    pub fn get_number(&self) -> &BigUint {
-        &self.num
+    pub fn get_number(&self) -> i64 {
+        self.num
     }
 
-    pub fn set_number(&mut self, new_num: BigUint) {
+    pub fn set_number(&mut self, new_num: i64) {
         self.num = new_num;
     }
 
-    pub fn zero() -> Self {
-        FieldElement::new(BigUint::zero()).unwrap()
+    pub fn set_prime(&mut self, new_prime: i64) {
+        self.prime = new_prime
     }
 
-    pub fn to_the_power_of(&self, exponent: BigUint) -> Self {
-        let exp = exponent % (self.get_prime() - BigUint::from_u64(1u64).unwrap());
-        let new_num = Self::mod_pow(self.num.clone(), exp.into(), &self.prime);
-        FieldElement::new(new_num).unwrap()
-    }
-
-    // credit to https://rob.co.bb/posts/2019-02-10-modular-exponentiation-in-rust/
-    fn mod_pow(mut base: BigUint, mut exp: BigUint, modulus: &BigUint) -> BigUint {
-        if *modulus == BigUint::one() {
-            return BigUint::zero();
-        }
-        let mut result = BigUint::one();
-        base = base % modulus;
-        while exp > BigUint::zero() {
-            if &exp % BigUint::from_u64(2u64).unwrap() == BigUint::one() {
-                result = result * &base % modulus;
-            }
-            exp = exp >> 1;
-            base = base.clone() * base % modulus
-        }
-        result
+    pub fn pow(&self, exponent: u32) -> Self {
+        let n = exponent % (self.get_prime() as u32 - 1);
+        let new_num = self.get_number().pow(n) % self.get_prime();
+        FieldElement::new(new_num, self.get_prime())
     }
 }
 
@@ -91,138 +59,142 @@ impl<'a> PartialEq<&'a FieldElement> for FieldElement {
 
 impl Eq for FieldElement {}
 
+
 impl Display for FieldElement {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "FieldElement_{:64} ({:64})", self.prime.to_str_radix(16), self.num.to_str_radix(16))
+        write!(f, "FieldElement_{} ({})", self.get_prime(), self.get_number())
     }
 }
 
 impl Add for FieldElement {
-    type Output = Result<FieldElement, Box<dyn Error>>;
+    type Output = FieldElement;
 
-    fn add(self, other: Self) -> Self::Output {
-        if self.prime != other.prime {
-            return Err("Cannt add two numbers in different Field.".into());
+    fn add(self, rhs: Self) -> Self::Output {
+        if &self.prime != &rhs.prime {
+            panic!("Cannot add two numbers in different Field.");
         }
-        let new_num = (self.num + other.num) % self.prime;
-        Ok(FieldElement::new(new_num)?)
+        let new_num = (self.get_number() + rhs.get_number()) % self.get_prime();
+        FieldElement::new(new_num, self.get_prime())
     }
 }
 
 impl<'a> Add<&'a FieldElement> for &'a FieldElement {
-    type Output = Result<FieldElement, Box<dyn Error>>;
+    type Output = FieldElement;
 
     fn add(self, rhs: &'a FieldElement) -> Self::Output {
-        if self.prime != rhs.prime {
-            return Err("Cannt add two numbers in different Field.".into());
-        }
-        let new_num = (&self.num + &rhs.num) % &self.prime;
-        Ok(FieldElement::new(new_num)?)
-    }
-} 
-
-impl Sub for FieldElement {
-    type Output = Result<FieldElement, Box<dyn Error>>;
-
-    fn sub(self, rhs: Self) -> Self::Output {
         if &self.prime != &rhs.prime {
-            return Err("Cannt add two numbers in different Field.".into());
+            panic!("Cannot add two numbers in different Field.");
         }
-
-        let mut new_num = BigUint::zero();
-        if &self.num >=  &rhs.num {
-            new_num = (self.get_number() - rhs.get_number()) % self.get_prime();
-        } else {
-            let ne_diff = (rhs.get_number() - self.get_number()) % self.get_number();
-            if ne_diff != BigUint::zero() {
-                new_num = self.get_prime() - ne_diff;
-            }
-        }
-        Ok(FieldElement::new(new_num)?)
+        let new_num = (self.get_number() + rhs.get_number()) % self.get_prime();
+        FieldElement::new(new_num, self.get_prime())
     }
 }
 
+impl Sub for FieldElement {
+    type Output = FieldElement;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        if &self.prime != &rhs.prime {
+            panic!("Cannot subtract two numbers in different Field.");
+        }
+
+        let diff_num = (self.get_number() - rhs.get_number()) % self.get_prime();
+        if diff_num >= 0 {
+            return FieldElement::new(diff_num, self.get_prime());
+        } else {
+            return FieldElement::new(self.get_prime() - diff_num, self.get_prime());
+        }
+     }
+}
+
 impl<'a> Sub<&'a FieldElement> for &'a FieldElement {
-    type Output = Result<FieldElement, Box<dyn Error>>;
+    type Output = FieldElement;
 
     fn sub(self, rhs: &'a FieldElement) -> Self::Output {
         if &self.prime != &rhs.prime {
-            return Err("Cannt add two numbers in different Field.".into());
+            panic!("Cannot subtract two numbers in different Field.");
         }
-        
-        let mut new_num = BigUint::zero();
-        if &self.num >= &rhs.num {
-            new_num = (self.get_number() - rhs.get_number()) % self.get_prime();
+
+        let diff_num = (self.get_number() - rhs.get_number()) % self.get_prime();
+        if diff_num >= 0 {
+            return FieldElement::new(diff_num, self.get_prime());
         } else {
-            let ne_diff = (rhs.get_number() - self.get_number()) % self.get_prime();
-            if ne_diff != BigUint::zero() {
-                new_num = self.get_prime() - ne_diff;
-            }
+            return FieldElement::new(self.get_prime() - diff_num, self.get_prime());
         }
-        Ok(FieldElement::new(new_num)?)
     }
 }
 
 impl Mul for FieldElement {
-    type Output = Result<FieldElement, Box<dyn Error>>;
+    type Output = FieldElement;
 
     fn mul(self, rhs: Self) -> Self::Output {
-        let new_num = self.get_number() * rhs.get_number();
-        Ok(FieldElement::new(new_num)?)
+        if &self.prime != &rhs.prime {
+            panic!("Cannot multiply two numbers in different Field.");
+        }
+        let new_num = self.get_number() * rhs.get_number() % self.get_prime();
+        FieldElement::new(new_num, self.get_prime())
     }
 }
 
 impl<'a> Mul<&'a FieldElement> for &'a FieldElement {
-    type Output = Result<FieldElement, Box<dyn Error>>;
+    type Output = FieldElement;
 
     fn mul(self, rhs: &'a FieldElement) -> Self::Output {
-        let new_num = self.get_number() * rhs.get_number();
-        Ok(FieldElement::new(new_num)?)
+        if &self.prime != &rhs.prime {
+            panic!("Cannot multiply two numbers in different Field.");
+        }
+        let new_num = self.get_number() * rhs.get_number() % self.get_prime();
+        FieldElement::new(new_num, self.get_prime())
     }
 }
 
-// impl Div for FieldElement {
-//     type Output = Self;
+impl Div for FieldElement {
+    type Output = FieldElement;
 
-//     fn div(self, rhs: Self) -> Self::Output {
-        
+    fn div(self, rhs: Self) -> Self::Output {
+        if &self.get_prime() != &rhs.get_prime() {
+            panic!("Cannot divide two numbers in different Field.");
+        }
+
+        let new_num = self.get_number() * rhs.get_number().pow(self.get_prime() as u32 - 2) % self.get_prime();
+        return FieldElement::new(new_num, self.get_prime())
+    }
+}
+
+impl<'a> Div<&'a FieldElement> for &'a FieldElement {
+    type Output = FieldElement;
+    
+    fn div(self, rhs: &'a FieldElement) -> Self::Output {
+        if &self.get_prime() != &rhs.get_prime() {
+            panic!("Cannot divide two numbers in different Field.");
+        }
+
+        let new_num = self.get_number() * rhs.get_number().pow(self.get_prime() as u32 - 2) % self.get_prime();
+        return FieldElement::new(new_num, self.get_prime())
+    }
+}
+
+// impl Rem for FieldElement {
+//     type Output = FieldElement;
+
+//     fn rem(self, rhs: Self) -> Self::Output {
+//         if &self.get_prime() != &rhs.get_prime() {
+//             panic!("Cannot oprate remainder between two numbers in different Field.");
+//         }
+
 //     }
 // }
-
 
 #[cfg(test)]
 mod field_element_tests {
     use super::*;
-    use num::Num;
-
-    const BIG1: &str = "79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798";
-    const B1PLUS1: &str = "79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81799";
-    const BIG2: &str = "483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8";
-
-    #[test]
-    fn confirm_secp256k1() {
-        let gx = BigUint::from_str_radix(BIG1, 16).unwrap();
-        let gy = BigUint::from_str_radix(BIG2, 16).unwrap();
-        let p: BigUint = BigUint::from_u8(2).unwrap().pow(256_u32)
-            - BigUint::from_u8(2).unwrap().pow(32_u32)
-            - BigUint::from_u32(977).unwrap();
-
-        assert_eq!(gy.pow(2_u32) % &p, (gx.pow(3_u32) + 7_u32) % p)
-    }
+ 
 
     #[test]
     fn field_element_eq() {
-        let a = FieldElement::new(
-            BigUint::from_str_radix(BIG1, 16).unwrap()
-        ).unwrap();
-
-        let b = FieldElement::new(
-            BigUint::from_str_radix(BIG1, 16).unwrap()
-        ).unwrap();
-
-        let c = FieldElement::new (
-            BigUint::from_str_radix(BIG2, 16).unwrap()
-        ).unwrap();
+        let a = FieldElement::new(100, 100);
+        let b = FieldElement::new(100, 100);
+        let c = FieldElement::new(100, -100);
 
         assert!(a == b);
         assert!(a != c);
@@ -231,88 +203,20 @@ mod field_element_tests {
     }
 
     #[test]
-    fn field_element_add_sub() -> Result<(), Box<dyn Error>>{
-        let a = FieldElement::new(
-            BigUint::from_str_radix(BIG1, 16).unwrap()
-        ).unwrap();
-
-        let b = FieldElement::new(
-            BigUint::from_str_radix("1", 16).unwrap(),
-        ).unwrap();
-
-        let c = FieldElement::new (
-            BigUint::from_str_radix(B1PLUS1, 16).unwrap()
-        ).unwrap();
-
-        assert!((a.clone() + b.clone())? == c.clone());
-        assert!((a.clone() - c.clone())? == (FieldElement::new(b.clone().get_prime() - b.clone().get_number())?));
-        
-        let ref_a = &a;
-        let ref_b = &b;
-        let ref_c = &c;
-        let added = (ref_a + ref_b)?;
-        let subtract = (ref_a - ref_c)?;
-        assert!(added == c);
-        assert!(subtract == (FieldElement::new(b.clone().get_prime() - b.clone().get_number()))?);
-        Ok(())
+    fn field_element_add_sub() {
+   
     }
 
     #[test]
-    fn field_element_mul_div() -> Result<(), Box<dyn Error>>{
-        let a = FieldElement::new(
-            BigUint::from_str_radix(BIG1, 16).unwrap()
-        ).unwrap();
+    fn field_element_mul_div() {
 
-        let c = FieldElement::new (
-            BigUint::from_str_radix("1", 16).unwrap()
-        ).unwrap();
-
-        assert!((a.clone() * c.clone())? == a.clone());
-        assert!(&(&a * &c)? == &a );
-        Ok(())
     }
 
-    #[test]
-    fn pow_test1() {
-        let a = FieldElement::new(
-            BigUint::from_str_radix(BIG1, 16).unwrap()
-        ).unwrap();
-
-        let b = a.clone();
-        a.to_the_power_of(BigUint::from_i8(1).unwrap());
-
-        assert_eq!(a, b);
-    }
-
-    #[test]
-    fn pow_test2() {
-        let a2 = FieldElement::new(
-            BigUint::from_i8(2).unwrap()
-        ).unwrap();
-
-        let a8 = FieldElement::new(
-            BigUint::from_i8(8).unwrap()
-        ).unwrap();
-
-        a2.to_the_power_of(BigUint::from_i8(3).unwrap());
-        assert_eq!(a2, a8);
-    }
 
     #[test]
     fn xxx() {
-        #[derive(Debug)]
-        struct MyStruct {
-            value: i32,
-        }
 
-        let a = MyStruct { value: 5 };
-        let b = MyStruct { value: 10 };
-        let ref_a = &a;
-        let ref_b = &b;
-        let result = ref_a.value + ref_b.value;
-        println!("Result: {}", result);
-        println!("a: {:?}", a); // 이후에도 여전히 a와 b를 사용할 수 있음
-        println!("b: {:?}", b);
     }
 
+ 
 }
--- basic_bitcoin/src/secp256k1/point.rs
+++ basic_bitcoin/src/secp256k1/point.rs
@@ -6,27 +6,26 @@
 use std::error::Error;
 use std::ops::{Add, Sub, Mul, Rem, Div};
 use std::fmt::Display;
-use num::{BigUint, FromPrimitive};
-use crate::secp256k1::field_element::FieldElement;
+
 
 #[derive(Clone, Debug)]
-struct Secp256k1Point {
-    a: FieldElement,
-    b: FieldElement,
-    x: Option<FieldElement>,
-    y: Option<FieldElement>,
+struct Point {
+    a: i32,
+    b: i32,
+    x: Option<i32>,
+    y: Option<i32>,
 }
 
-impl Secp256k1Point {
-    fn new(a: FieldElement, b: FieldElement, x: Option<FieldElement>, y: Option<FieldElement>) 
+impl Point {
+    fn new(x: Option<i32>, y: Option<i32>, a: i32, b: i32) 
         -> Result<Self, Box<dyn Error>> {
             let err_msg = "x, y is not on the curve.";
 
             match (&x, &y) {
                 (None, None) => Ok(Self { a, b, x, y }),
                 (Some(x_value), Some(y_value)) => {
-                    if y_value.to_the_power_of(BigUint::from_i8(2).unwrap()) == 
-                    ((x_value.to_the_power_of(BigUint::from_i8(3).unwrap()) + (a.clone() * x_value.clone())?)? + b.clone())? {
+                    if y_value.pow(2) == 
+                        x_value.pow(3) + (a * x_value) + b {
                         return Ok(Self { a, b, x, y });
                     } else {
                         return Err(err_msg.into());
@@ -37,7 +36,7 @@ impl Secp256k1Point {
     }
 }
 
-impl PartialEq for Secp256k1Point {
+impl PartialEq for Point {
     fn eq(&self, other: &Self) -> bool {
         if self.a == other.a && self.b == other.b && self.x == other.x && self.y == other.y {
             return true;
@@ -47,8 +46,19 @@ impl PartialEq for Secp256k1Point {
     }
 }
 
+impl Eq for Point {}
 
-impl Display for Secp256k1Point {
+impl<'a> PartialEq<&'a Point> for Point {
+    fn eq(&self, other: &&'a Point) -> bool {
+        if self.a == other.a && self.b == other.b && self.x == other.x && self.y == other.y {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+impl Display for Point {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
@@ -58,67 +68,68 @@ impl Display for Secp256k1Point {
     }
 }
 
+impl Add for Point {
+    type Output = Result<Point, Box<dyn Error>>;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        if &self.a != &rhs.a || &self.b != &rhs.b {
+            panic!("They are not on the same curve.")
+        } else if self.x.is_none() || self.y.is_none() {
+            return Ok(rhs.clone());
+        } else if rhs.x.is_none() || rhs.y.is_none() {
+            return Ok(self.clone());
+        } else {
+            if let (Some(s_x), Some(s_y), Some(r_x), Some(r_y)) = (&self.x, &self.y, &rhs.x, &rhs.y) {
+                if s_x == r_x && s_y != r_y {
+                    return Point::new(None, None, self.a, self.b);
+                }
+                if s_x != r_x {
+                    let s = (r_y - s_y) / (r_x - s_x);
+                    let x = s.pow(2) - s_x - r_x;
+                    let y = s * (s_x - x) - s_y;
+                    
+                    return Point::new(Some(x), Some(y), self.a, self.b);
+                }
+                if self == rhs && s_y == &(0 * s_x) {
+                    return Point::new(None, None, self.a, self.b);
+                }
+                if self == rhs {
+                    let s = (3 * s_x.pow(2) + self.a) / (2 + s_y);
+                    let x = s.pow(2) - 2 * s_x;
+                    let y = s * (s_x -  x) - s_y;
+    
+                    return Point::new(Some(x), Some(y), self.a, self.b);
+                }
+            };
+            return Err("Out of case".into());
+        } 
+    }
+}
 
 #[cfg(test)]
-mod secp256k1point_test {
+mod point_test {
     use super::*;
-    use num::BigUint;
-
-    type FE = FieldElement;
-    type PNT = Secp256k1Point;
-
-    const BIG1: &str = "79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798";
-    const B1PLUS1: &str = "79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81799";
-    const BIG2: &str = "483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8";
 
     #[test]
     fn new_test() -> Result<(), Box<dyn Error>>{
-        let _a = PNT::new(
-            FE::new(BigUint::from_u8(1).unwrap())?,
-            FE::new(BigUint::from_u8(1).unwrap())?,
-            None,
-            None,
-        )?;
+        let p1 = Point::new(Some(-1), Some(-1), 5, 7)?;
+        let p1_eq = Point::new(Some(-1), Some(-1), 5, 7)?;
+        let p2 = Point::new(Some(-1), Some(1), 5, 7)?;
+        let inf =  Point::new(None, None, 5, 7)?;
         
-        // let b = PNT::new(
-        //     FE::new(BigUint::from_u8(1).unwrap())?,
-        //     FE::new(BigUint::from_u8(1).unwrap())?,
-        //     Some(FE::new(BigUint::from_u8(1).unwrap())?),
-        //     None,
-        // )?; //Error 발생
+        assert!(p1 == p1_eq);
+        assert!(p1 != p2);
+        
 
-        let _on_elliptic = PNT::new(
-            FE::new(BigUint::from_u8(1).unwrap())?,
-            FE::new(BigUint::from_u8(2).unwrap())?,
-            Some(FE::new(BigUint::from_u8(1).unwrap())?),
-            Some(FE::new(BigUint::from_u8(2).unwrap())?),
-        )?;
+        assert!(&(p1.clone() + inf.clone())? == &p1);
+        assert!(&(inf.clone() + p2.clone())? == &p2);
+        assert!(&(p1.clone() + p2.clone())? == &inf);
+
         Ok(())
     }
 
     #[test]
-    fn ep_test() -> Result<(), Box<dyn Error>> {
-        let a = PNT::new(
-            FE::new(BigUint::from_u8(1).unwrap())?,
-            FE::new(BigUint::from_u8(2).unwrap())?,
-            Some(FE::new(BigUint::from_u8(1).unwrap())?),
-            Some(FE::new(BigUint::from_u8(2).unwrap())?),
-        )?;
-        let b = PNT::new(
-            FE::new(BigUint::from_u8(1).unwrap())?,
-            FE::new(BigUint::from_u8(2).unwrap())?,
-            Some(FE::new(BigUint::from_u8(1).unwrap())?),
-            Some(FE::new(BigUint::from_u8(2).unwrap())?),
-        )?;
-        let c = PNT::new(
-            FE::new(BigUint::from_u8(1).unwrap())?,
-            FE::new(BigUint::from_u8(7).unwrap())?,
-            Some(FE::new(BigUint::from_u8(1).unwrap())?),
-            Some(FE::new(BigUint::from_u8(3).unwrap())?),
-        )?;
+    fn ep_test() {
 
-        assert!(a == b);
-        assert!(a != c);
-        Ok(())
     }
 }
