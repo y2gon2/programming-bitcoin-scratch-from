8a29b11cb62956f5ea2c4405ec5b5203a0767c68
y2gon2 <y2gon2@gmail.com>
2023-07-08 19:51:33 +0900

    Working: Transaction

--- basic_bitcoin/src/models/helper.rs
+++ basic_bitcoin/src/models/helper.rs
@@ -1,7 +1,10 @@
 //! collection of functions
 
+use std::io::{Read};
+use std::error::Error;
 
-fn litte_endian_to_u32(four_u8: &[u8; 4]) -> u32 {
+// 입력된 u8 4개의 입력값을 litte endian serialize 로 변환 및 계산된 u32 값으로 변환
+pub fn little_endian_to_u32(four_u8: &Vec<u8>) -> u32 {
     let mut result: u32 = 0;
 
     for (idx, &byte) in four_u8.iter().enumerate() {
@@ -10,17 +13,109 @@ fn litte_endian_to_u32(four_u8: &[u8; 4]) -> u32 {
     result
 } 
 
+pub fn u32_to_little_endian(input: u32, len: u8) -> Result<Vec<u8>, Box<dyn Error>> {
+    match len {
+        2 => {
+            let bytes = (input as u16).to_le_bytes();
+            Ok(bytes.to_vec())
+        },
+        4 => {
+            let bytes = input.to_le_bytes();
+            Ok(bytes.to_vec())
+        },
+        _ => Err("Invalid length".into())
+    }
+}
+
+// 가변 정수 표현
+// 다음의 규칙으로 0 ~ (2^64 - 1) 사이의 숫자를 표현
+//
+//  정수 범위                 Variats 표현 방법                             예
+//    0~252            1 byte                                  100 -> 0x64 (일반적 표현 방시과 동일)
+// 253 ~ 2^16-1        접두부 0xfd 이후 2bytes little endian    255 -> 0xfdff00
+//                                                             555 -> 0xfd2b02              
+// 2^16 ~ 2^32-1       접두부 0xfe 이후 4bytes little endian    70015 -> 0xfe7f110100 
+// 2^32 ~ 2^64-1       접두부 0xff 이후 8bytes little endian    81005558675309 -> 0xff6dc7ed3e60100000
+
+// stream 으로 부터 필요한 bytes 만큼 읽고 정수로 반환
+pub fn read_varint<R: Read>(reader: &mut R) -> Result<u64, std::io::Error> {
+    let mut buf = [0u8; 1];
+    reader.read_exact(&mut buf)?;
+    let i = buf[0];
+
+    let mut buf = [0u8; 8];
+    match i {
+        0xfd => {
+            let mut read_buf = [0u8; 2];
+            reader.read_exact(&mut read_buf)?;
+
+            buf[0] = read_buf[0];
+            buf[1] = read_buf[1];
+
+            Ok(u64::from_le_bytes(buf))
+        },
+        0xfe => {
+            let mut read_buf = [0u8; 4];
+            reader.read_exact(&mut read_buf)?;
+
+            buf[0] = read_buf[0];
+            buf[1] = read_buf[1];
+            buf[2] = read_buf[2];
+            buf[3] = read_buf[3];
+
+            Ok(u64::from_le_bytes(buf))
+        },
+        0xff => {
+            reader.read_exact(&mut buf)?;
+
+            Ok(u64::from_le_bytes(buf))
+        },
+        _ => {
+            Ok(u64::from(i))
+        },
+    }
+}
+
+// read_varint 와 반대로 정수값을 받아 varint 형식으로 변환된 bytes 를 반환
+pub fn encode_varint(input: u32) -> Result<Vec<u8>, Box<dyn Error>> {
+    let mut v = Vec::<u8>::new();
+
+    if  input < 0xfd {
+        v.push(input as u8);
+        Ok(v)
+    } else if input < 0x10000 {
+        v.push(0xfd);
+        v.append(&mut u32_to_little_endian(input, 2)?);
+        Ok(v)
+    } else if input < 0xffffffff {
+        v.push(0xfe);
+        v.append(&mut u32_to_little_endian(input, 4)?);
+        Ok(v)
+    // } else if input < 0x10000000000000000 {
+    //     v.push(input as u8); // 현재 입력 가능 값이 u32 로 해당 값은 범위를 초과함.
+    //     Ok(v)
+    } else {
+        Err("input value is too large".into())
+    }
+
+}
+ 
 
 #[cfg(test)]
 mod hleper_test {
-    use super::litte_endian_to_u32;
-
+    use super::*;
 
     #[test]
     fn litte_endian() {
-        let byte_sequence: [u8; 4] = [0xff, 0x00, 0x00, 0x00];
-        let result: u32 = litte_endian_to_u32(&byte_sequence);
-
-        println!("{}", result); // 255
+        let mut data: &[u8] = &[0xfd, 0x01, 0x02, 0xfe, 0x03, 0x04, 0x05, 0x06, 0xff, 0x07, 0x08, 0x09, 0x0a];
+        let mut data2: &[u8] = &[0xff, 0x6d, 0xc7, 0xed, 0x3e, 0x60, 0x10, 0x00, 0x00, 0x64];
+        let num1 = read_varint(&mut data).unwrap();
+        let num2 = read_varint(&mut data).unwrap();
+        let num3 = read_varint(&mut data2).unwrap();
+        let num4 = read_varint(&mut data2).unwrap();
+        println!("num1: {}", num1);
+        println!("num2: {}", num2);
+        println!("num3: {}", num3);
+        println!("num4: {}", num4);
     }
 }
--- basic_bitcoin/src/models/tx.rs
+++ basic_bitcoin/src/models/tx.rs
@@ -1,26 +1,30 @@
+use std::error::Error;
 use std::fmt::Display;
 use std::hash::{Hash, Hasher};
 use std::collections::hash_map::DefaultHasher;
-use std::io::{stdin, Read};
+use std::io::{stdin, stdout, Read, Write};
 
 use crate::models::helper::*;
 
-#[derive(Hash)]
+type version = u32;
+type locktime = u32;
+
+#[derive(Hash, Debug, Clone)]
 pub struct Tx {
-    version: u32,   // transaction version
-    tx_ins: Option<Vec<u32>>,    // 사용할 bitcoin 정의
-    tx_outs: Option<Vec<u32>>,   // bitcoin 이 전달될 목적지
-    locktime: Option<u32>,  // tranaction 유효 시점
+    version: version,   // transaction version
+    tx_ins: Option<Vec<TxIn>>,    // 사용할 bitcoin 정의
+    tx_outs: Option<Vec<TxOut>>,   // bitcoin 이 전달될 목적지
+    locktime: Option<locktime>,  // tranaction 유효 시점
     testnet: bool,   // transaction 을 검증하기 위해서 어떤 Network (ex. testnet, mainnet 등) 에서 발생했는지 명
 }
 
 impl Tx {
 
     pub fn new(
-        version: u32, 
-        tx_ins: Option<Vec<u32>>, 
-        tx_outs: Option<Vec<u32>>, 
-        locktime: Option<u32>, 
+        version: version, 
+        tx_ins: Option<Vec<TxIn>>, 
+        tx_outs: Option<Vec<TxOut>>, 
+        locktime: Option<locktime>, 
     ) -> Self {
         Self {
             version,
@@ -45,11 +49,44 @@ impl Tx {
     // version - serialized bytes [u8; 4] array 를 입력받으면 
     // 이를 little endian 으로 parsing -> u32 로 변환하여 이를 version value 로 사용하는
     // Tx instance 생성
-    pub fn parse() {
-        let mut buf = String::new();
-        let _ = stdin().lock().read_to_string(&mut buf);
+    pub fn parse<R: Read>(reader: &mut R, testnet: bool) -> Result<Self, Box<dyn Error>>{
+        // Stream 에서 version(4 bytes, little-endian) 읽기
+        let mut version_bytes = [0u8; 4];
+        reader.read_exact(&mut version_bytes)?;
+        let version = version::from_le_bytes(version_bytes);
 
-        
+        // Stream 에서  num_inputs (helper::varint) 읽기
+        let num_inputs = read_varint(reader)?;
+
+        // Parse num_inputs number of TxIns (검토 및 재작성) 
+        // let mut inputs = Vec::with_capacity(num_inputs as usize);
+        // for _ in 0..num_inputs {
+        //     let input = TxIn::parse(reader)?;
+        //     input.push(input);
+        // }
+
+        // Stream 에서 num_outputs (hepler::varint) 읽기
+        let num_outputs = read_varint(reader)?;
+
+        // Parse num_outputs number of TxIns (검토 및 재작성)
+        // let output = Vec::with_capacity(num_outputs as usize);
+        // for _ in 0..num_outputs {
+        //      let output = TxOut::parse(reader)?;
+        //      output.push(output);
+        // }
+
+        // Stream 에서 locktime (4 bytes, little-endian) 읽기
+        let mut locktime_bytes = [0u8; 4];
+        reader.read_exact(&mut locktime_bytes)?;
+        let locktime = locktime::from_le_bytes(locktime_bytes);
+
+        Ok(Self {
+            version,
+            tx_ins: Some(input),   
+            tx_outs: Some(output),  
+            locktime: Some(locktime), 
+            testnet: false,
+        })
     }
  }
 
@@ -82,3 +119,118 @@ impl Display for Tx {
         )
     }
 }
+
+type PrevTx = [u8; 32];
+type PrevIndex = u32;
+type ScriptSig = Vec<u8>; 
+type Sequence = u32;
+
+
+#[derive(Hash, Debug, Clone)]
+pub struct TxIn {
+    prev_tx: PrevTx,
+    // 이전 transaction identifier (TXID) 
+    // transaction data hashing 으로 생성되어 TX 각각 고유한 값을 가짐.
+    // 특정 TX 의 추적, 검색에 사용 가능
+
+    prev_index: PrevIndex,
+    // 이전 transaction 에서 여러 출력이 발생한 경우 이에 대한 output index
+    // 예를 들면 Bob 이 Alice 와 Tom 에게 한번의 TX 에서 둘다 bitcoin  전송이 가능하며,
+    // 이 때 각각의 구분하기 위한 index 가 필요하다. 
+    // index 값은 0 부터 시작 한다. 
+    
+    script_sig: Option<ScriptSig>,
+    // 이전 출력의 잠금 script 를 해제하기 위한 입력 script 
+    // 일반적으로 송신자의 디지털 서명고 공개키를 포함하며, 이것으로 이전 출력이 
+    // 해당 사용자에게 속하는 것을 증명하고, 그것을 사용할 권리가 있음을 보여줌
+    //
+    // "이전 출력의 잠금 script 를 해제" 작업이 필요한 이유
+    // -> bitcoin 소유권을 보장하고 이중지불(double-spending)을 방지하기 위함
+    //    
+    // UTXO : Unspent Transaction Output (미사용 거래 출력)
+    //  - 새로운 bitcoin TX 를 만들때, 이전 UTXO 를 입력으로 사용한다. 
+    //    이때, 그 UTXO를 사용할 수 있는 권한을 증명해야 하는데, 
+    //    이때 필요한 것이 바료 이전 출력의 잠금 script를 해제하는 것
+    //    (나에게 입력된 TX 중 소비되지 않은 TX)
+
+    sequence: Sequence,
+    // bitcoin time-lock  기능을 지원하는데 사용
+    // transaction 의 특정 block 높이 또는 시간이 될 때까지 승이되지 않도록 하는데 사용
+    //
+    // 최초 transaction 을 업데이트하거나 대체하기 위한 메카니즘을 제공하기 위해 도입
+    // 그러나 bitcoin 초기 버젼에는 활성화 되어 있지 않음. 
+}
+
+impl TxIn {
+    pub fn new(
+        prev_tx: PrevTx, 
+        prev_index: PrevIndex, 
+        script_sig: Option<ScriptSig>, 
+    ) -> Self {
+        Self {
+            prev_tx,
+            prev_index,
+            script_sig,
+            sequence: 0xffffffff,
+        }
+    } 
+
+    pub fn parse<R: Read>(reader: R) -> Result<Self, Box<dyn Error>> {
+        
+        // prev_tx (32bytes, little-endian)
+        let mut prev_tx = [0u8; 32];
+        reader.read_exact(&mut prev_tx)?;
+        prev_tx.reverse();
+
+        // prev_index : u32 from 4 bytes, little-endian
+        let mut prev_index_bytes = [0u8; 4];
+        reader.read_exact(&mut prev_index_bytes)?;
+        
+        let  prev_index = PrevIndex::from_le_bytes(prev_index_bytes);
+
+        // let script_sig =  // to do
+
+        let mut seq_bytes = [0u8; 4];
+        reader.read_exact(&mut seq_bytes)?;
+
+        let sequence = Sequence::from_le_bytes(seq_bytes);
+
+        Ok(Self {
+            prev_tx,
+            prev_index,
+            script_sig,
+            sequence,
+        })
+    }
+}
+
+impl Display for TxIn {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut hex = String::new();
+
+        write!(
+            f,
+            "{:02X?}: {}",
+            self.prev_tx, 
+            self.prev_index,
+        )
+    }
+}
+
+
+#[cfg(test)]
+mod tx_test {
+    use super::*;
+    use std::prelude::*;
+
+
+    #[test]
+    fn parsing_test() -> Result<(), Box<dyn Error>>{
+        let t1 = Tx::parse()?;
+        println!("{:?}", &t1);
+        Ok(())
+    }
+}
+
+#[derive(Hash, Debug, Clone)]
+pub struct TxOut {}
