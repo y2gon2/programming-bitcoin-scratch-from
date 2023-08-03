4689a60f81252369e745a22d06778b8301d01439
y2gon2 <y2gon2@gmail.com>
2023-08-03 21:41:02 +0900

    completed: network.rs

--- basic_bitcoin/src/models/network.rs
+++ basic_bitcoin/src/models/network.rs
@@ -1,15 +1,23 @@
 
-use std::io::Read;
+use std::io::{Read, BufReader, Write};
 use std::error::Error;
 use std::time::{SystemTime, UNIX_EPOCH};
+use std::net::TcpStream;
+use std::collections::HashMap;
 
 use rand::{thread_rng, Rng};
+use log::info;
 
 use crate::models::helper::*;
+use crate::models::block::Block;
 
 const NETWORK_MAGIC: [u8; 4] = [0xf9, 0xbe, 0xb4, 0xd9];
 const TESTNET_NETWORK_MAGIC: [u8; 4] = [0x0b, 0x11, 0x09, 0x07];
 
+
+//------------------------------------------------------------------------------
+//                                 NetworkEnvelope
+//------------------------------------------------------------------------------
 
 /// P2P network 통신 패킷  전송 내용인 payload 를 포함하고 있는 Network Message
 #[derive(Clone, Debug)]
@@ -186,6 +194,18 @@ mod test_networkenvelope {
     }
 }
 
+//------------------------------------------------------------------------------
+//                                 Message trait
+//------------------------------------------------------------------------------
+
+pub trait Message {
+    fn command(&self) -> Vec<u8>;
+    fn serialize(&self) -> Vec<u8>;
+    fn parse<R: Read>(reader: &mut R) -> Self where Self: Sized;
+}
+//------------------------------------------------------------------------------
+//                                 VersionMessage
+//------------------------------------------------------------------------------
 
 /// payload 구성
 /// 
@@ -229,7 +249,7 @@ pub struct VersionMessage {
 }
 
 impl VersionMessage {
-    pub const COMMAND: &'static str = "version";
+    // pub const COMMAND: &'static str = "version";
 
     pub fn new(
         version: u32, services: u64, timestamp: Option<u64>,
@@ -280,9 +300,35 @@ impl VersionMessage {
             relay
         }
     }
+}
+
+impl Default for VersionMessage {
+    fn default() -> Self {
+        Self {
+            version:70015,
+            services: 0,
+            timestamp: 0,
+            receiver_services: 0,
+            receiver_ip: [0, 0, 0, 0],
+            receiver_port: 8333,
+            sender_services: 0,
+            sender_ip: [0, 0, 0, 0],
+            sender_port: 8333,
+            nonce: [0, 0, 0, 0, 0, 0, 0, 0],
+            user_agent: vec![0x18, b'/', b'p', b'r', b'o', b'g', b'r', b'a', b'm', b'm', b'i', b'n', b'g', b'b', b'i', b't', b'c', b'o', b'i', b'n', b':', b'0', b'.', b'1', b'/'],
+            latest_block: 0,
+            relay: false,
+        }
+    }
+}
+
+impl Message for VersionMessage {
+    fn command(&self) -> Vec<u8> {
+        vec![b'v', b'e', b'r', b's', b'i', b'o', b'n']
+    }
 
     /// Serialize this message to send over the network
-    pub fn serialize(&self) -> Vec<u8> {
+    fn serialize(&self) -> Vec<u8> {
         let mut unused_ipv6 = vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255];
 
         let mut result = Vec::<u8>::new();
@@ -329,29 +375,14 @@ impl VersionMessage {
 
         result
     }
-}
 
-impl Default for VersionMessage {
-    fn default() -> Self {
-        Self {
-            version:70015,
-            services: 0,
-            timestamp: 0,
-            receiver_services: 0,
-            receiver_ip: [0, 0, 0, 0],
-            receiver_port: 8333,
-            sender_services: 0,
-            sender_ip: [0, 0, 0, 0],
-            sender_port: 8333,
-            nonce: [0, 0, 0, 0, 0, 0, 0, 0],
-            user_agent: vec![b'/', b'p', b'r', b'o', b'g', b'r', b'a', b'm', b'm', b'i', b'n', b'g', b'b', b'i', b't', b'c', b'o', b'i', b'n', b':', b'0', b'.', b'1', b'/'],
-            latest_block: 0,
-            relay: false,
-        }
+    #[allow(unused_variables)]
+    fn parse<R: Read>(reader: &mut R) -> Self {
+        Self::default()
     }
 }
 
-
+//------------------------------------------------------------------------------
 #[cfg(test)]
 mod version_message_test {
     use super::*;
@@ -361,7 +392,7 @@ mod version_message_test {
         let v = VersionMessage::default();
         let s = v.serialize();
 
-        let msg = "7f11010000000000000000000000000000000000000000000000000000000000000000000000ffff00000000208d000000000000000000000000000000000000ffff00000000208d00000000000000002f70726f6772616d6d696e67626974636f696e3a302e312f0000000000";
+        let msg = "7f11010000000000000000000000000000000000000000000000000000000000000000000000ffff00000000208d000000000000000000000000000000000000ffff00000000208d0000000000000000182f70726f6772616d6d696e67626974636f696e3a302e312f0000000000";
 
         let m = str_to_vec_u8(msg);
 
@@ -370,4 +401,407 @@ mod version_message_test {
         
         assert_eq!(s, m);
     }
+}
+
+//------------------------------------------------------------------------------
+//                                 VerAckMessage
+//------------------------------------------------------------------------------
+
+#[derive(Clone)]
+pub struct VerAckMessage ();
+
+impl VerAckMessage {
+    pub fn new() -> Self  {
+        Self()
+    } 
+}
+
+impl Message for VerAckMessage {
+    fn command(&self) -> Vec<u8> {
+        vec![b'v', b'e', b'r', b'a', b'c', b'k']
+    }
+
+    fn serialize(&self) -> Vec<u8> {
+        Vec::<u8>::new()
+    }
+
+    #[allow(unused_variables)]
+    fn parse<R: Read>(reader: &mut R) -> Self {
+        Self()
+    }
+}
+
+//------------------------------------------------------------------------------
+//                                PingMessage
+//------------------------------------------------------------------------------
+
+#[derive(Clone)]
+pub struct PingMessage {
+    nonce: [u8; 8]
+}
+
+impl PingMessage {
+    pub fn new(nonce: [u8; 8]) -> Self {
+        Self { nonce }
+    }
+}
+
+impl Message for PingMessage {
+    fn command(&self) -> Vec<u8> {
+        vec![b'p', b'i', b'n', b'g']
+    }
+
+    fn serialize(&self) -> Vec<u8> {
+        self.nonce.to_vec()
+    }
+    
+    fn parse<R: Read>(reader: &mut R) -> Self {
+        let mut nonce = [0u8; 8];
+        let _ = reader.read_exact(&mut nonce);
+
+        Self { nonce }
+    } 
+}
+
+//------------------------------------------------------------------------------
+//                                PongMessage
+//------------------------------------------------------------------------------
+
+#[derive(Clone)]
+pub struct PongMessage {
+    nonce: [u8; 8]
+}
+
+impl PongMessage {
+    pub fn new(nonce: [u8; 8]) -> Self {
+        Self { nonce }
+    }
+}
+
+impl Message for PongMessage {
+    fn command(&self) -> Vec<u8> {
+        vec![b'p', b'o', b'n', b'g']
+    }
+
+    fn serialize(&self) -> Vec<u8> {
+        self.nonce.to_vec()
+    }
+
+    fn parse<R: Read>(reader: &mut R) -> Self {
+        let mut nonce = [0u8; 8];
+        let _ = reader.read_exact(&mut nonce);
+
+        Self { nonce }
+    }
+}
+
+//------------------------------------------------------------------------------
+//                              GetHeaderMessage
+//------------------------------------------------------------------------------
+
+/// 처음 네트워크에 연걸 후 Block Header download 
+/// Block Header size 는 전체 Blockchain 의 0.023% 로 빠르게 다운로드 가능
+/// Block Header 를 먼저 받고 나머지 full node 는 이후 병렬 처리로 다운로드하여 효율적으로 처리 가능
+/// Light Node 의 경우 Block Header 만으로 작업 증명 가능
+/// 
+/// * getheader message payload 예시
+/// 7f11010001ad3...000000..000
+/// 
+/// 7f110100 (4 bytes, little-endian) : Protocol version (70015) 
+/// 01 : Number of hashes, varint
+/// a34b...000 (32 bytes, little-endian) : Starting block
+/// 0000...000 (32 bytes, little-endian) : End Block
+#[derive(Clone)]
+pub struct GetHeaderMessage {
+    version: u32,
+    num_hashes: u32,
+    start_block: [u8; 32],
+    end_block: [u8; 32],
+}
+
+impl GetHeaderMessage {
+    pub fn new(version: u32, num_hashes: u32, start_block: [u8; 32], end_block: [u8; 32]) -> Self {
+        Self {
+            version, 
+            num_hashes,
+            start_block,
+            end_block 
+        }   
+    }
+
+    pub fn default(start_block: [u8; 32]) -> Self {
+        Self {
+            version: 70015, 
+            num_hashes: 1,
+            start_block,
+            end_block: [0u8; 32],
+        }
+    }
+}
+
+
+impl Message for GetHeaderMessage {
+    fn command(&self) -> Vec<u8> {
+        vec![b'g', b'e', b't', b'h', b'e', b'a', b'd', b'e', b'r', b's']
+    }
+
+    /// Serialize this message to send over the network
+    fn serialize(&self) -> Vec<u8> {
+        let mut result = self.version.to_le_bytes().to_vec();
+
+        let mut num_hashes = encode_varint(self.num_hashes).unwrap();
+        result.append(&mut num_hashes);
+
+        let mut start_block = self.start_block.to_vec();
+        start_block.reverse();
+        result.append(&mut start_block);
+
+        let mut end_block = self.end_block.to_vec();
+        end_block.reverse();
+        result.append(&mut end_block);
+
+        result
+    }
+
+    #[allow(unused_variables)]
+    fn parse<R: Read>(reader: &mut R) -> Self where Self: Sized {
+        GetHeaderMessage::default([0u8; 32])
+    }
+}
+
+#[cfg(test)]
+mod get_header_message_test {
+    use super::*;
+
+    #[test]
+    fn test_serialize() {
+        let block_hex = "0000000000000000001237f46acddf58578a37e213d2a6edc4884a2fcad05ba3";
+        let block_vec = str_to_vec_u8(block_hex);
+        let start_block: [u8; 32] = block_vec.try_into().unwrap();
+
+        let gh = GetHeaderMessage::default(start_block);
+        assert_eq!(
+            str_to_vec_u8("7f11010001a35bd0ca2f4a88c4eda6d213e2378a5758dfcd6af437120000000000000000000000000000000000000000000000000000000000000000000000000000000000"),
+            gh.serialize()
+        )
+    }
+}
+
+//------------------------------------------------------------------------------
+//                               HeaderMessage
+//------------------------------------------------------------------------------
+
+/// header 요청의 응답으로 상대 노드에서 header commad 를 가진 message 를 보냄.
+/// 이 응답 message payload 에는 block header list 가 담겨 있으며, 
+/// 그 parsing 구조는 다음과 같다.
+/// 
+/// * Header Command Payload
+/// 020000020...157e67000000...de09204600 (1~2000 bytes 의 가변정수(varint))
+/// 02 (u8) : Number of Block Headers
+/// 00...67 : Block Header
+/// 00      : Number of transactions (항상 0)
+/// 
+/// 위 자료에서 transaction 숫자가 항상 0 인 것은 Header 에는 transation 정보를 담지 않기 때문
+/// 그렇다면, 왜 의미 없는 0x00 를 header 마다 붙여주는가?
+/// -> Header Message 를 block Message 의 format 과 호환되게 하기 위함 
+///    Block Message 의 경우 block header + transaction 수 + transation list 로 구성
+///    transaction 수를 0 으로 지정하면 block 전체 (header + transaction) 을 parsing 할 때와
+///    동일한 parsing 함수를 사용하는 장점이 있음 (이해 X)
+pub struct HeaderMessage {
+    blocks: Vec<Block>,
+}
+
+impl Message for HeaderMessage {
+    fn command(&self) -> Vec<u8> {
+        vec![ b'h', b'e', b'a', b'd', b'e', b'r', b's']
+    }
+
+    fn serialize(&self) -> Vec<u8> {
+        Vec::<u8>::new()
+    }
+
+    fn parse<R: Read>(reader: &mut R) -> Self where Self: Sized {
+        let num_headers = read_varint(reader).unwrap();
+        println!("{num_headers}");
+
+        let mut blocks = Vec::<Block>::new();
+
+        for _ in 0..num_headers {
+            blocks.push(Block::parse(reader));
+
+            // read next varint
+            let num_txs = read_varint(reader).unwrap();
+            if num_txs != 0 {
+                panic!("number of txs not 0")
+            }
+        }
+
+        Self { blocks }
+    }
+}
+
+#[cfg(test)]
+mod header_message_test {
+    use std::io::Cursor;
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let hex_msg = "0200000020df3b053dc46f162a9b00c7f0d5124e2676d47bbe7c5d0\
+            793a500000000000000ef445fef2ed495c275892206ca533e7411907971013ab83e3b47b\
+            d0d692d14d4dc7c835b67d8001ac157e670000000002030eb2540c41025690160a1014c5\
+            77061596e32e426b712c7ca00000000000000768b89f07044e6130ead292a3f51951adbd\
+            2202df447d98789339937fd006bd44880835b67d8001ade09204600";
+        
+        let msg_vec = str_to_vec_u8(hex_msg);
+        let mut cursor = Cursor::new(msg_vec);
+        
+        let headers = HeaderMessage::parse(&mut cursor);
+
+        assert_eq!(headers.blocks.len() , 2);
+    }
+}
+
+//------------------------------------------------------------------------------
+//                                 SimpleNode
+//------------------------------------------------------------------------------
+
+pub struct SimpleNode {
+    testnet: bool,
+    logging: bool,
+    socket: TcpStream,
+    stream: BufReader<TcpStream>,
+}
+
+impl SimpleNode {
+    pub fn new(host: &str, port: Option<u16>, testnet: bool, logging: bool) 
+    -> Result<Self, Box<dyn Error>> {
+        let port = match port {
+            Some(port) => port,
+            None => {
+                if testnet {
+                    18333
+                } else {
+                    8333
+                }
+            },
+        };
+
+        let socket = TcpStream::connect((host, port))?;
+        let stream = BufReader::new(socket.try_clone()?);
+
+        Ok(Self {
+            testnet,
+            logging,
+            socket,
+            stream,
+        })
+    } 
+
+    /// Do a handshake with the other node.
+    /// Handshake is sending a version message and getting a verack back.
+    pub fn handshake(&mut self) -> Box<dyn Message>{
+
+        // send a version message
+        let version = VersionMessage::default();
+        let _ = self.send(Box::new(version));
+
+        // wait fora verack message
+        self.wait_for(vec![Box::new(VerAckMessage::new())])
+    }
+
+    /// Send a message to the connected node
+    pub fn send(&mut self, message: Box<dyn Message>) -> Result<(), std::io::Error> {
+        let envelope = NetworkEnvelope::new(
+            vec![b'v', b'e', b'r', b's', b'i', b'o', b'n'],
+            message.serialize(), 
+            self.testnet
+        );
+
+        if self.logging {
+            info!("Sending: {:?}", &envelope);
+        }
+
+        self.socket.write_all(&envelope.serialize())
+    }
+
+    /// Read a message from the socket
+    pub fn read(&mut self) -> NetworkEnvelope{    
+        let envelope = NetworkEnvelope::parse(&mut self.stream, self.testnet).unwrap();
+
+        if self.logging {
+            info!("receiving: {:?}", &envelope);
+        }
+
+        return envelope
+    }
+
+    /// Wait for one of the messages in the list
+    pub fn wait_for(&mut self, messages: Vec<Box<dyn Message>>) -> Box<dyn Message> {
+        // initialize the command we have, which should be None
+        let mut command = Vec::<u8>::new();
+        let mut command_to_class = HashMap::<Vec<u8>, Box<dyn Message>>::new();
+        for m in messages {
+            command_to_class.insert(m.command(), m);
+        }
+
+        // loop until the command is in the commands we want
+        while !command_to_class.contains_key(&command) {
+
+            // get the next network message
+            let envelope = self.read();
+
+            // set the command to be evaluated
+            command = envelope.command;
+
+            // we know how to respond to version and ping, handle that here
+            if command == vec![b'v', b'e', b'r', b's', b'i', b'o', b'n'] {
+                // send verack
+                let _ = self.send(Box::new(VerAckMessage::new()));
+            } else if command == vec![b'p', b'i', b'n', b'g'] {
+                // send pong
+                let mut nonce = [0u8; 8];
+                if envelope.payload.len() == 8 {
+                    nonce.copy_from_slice(&envelope.payload);
+                } else {
+                    panic!("Length of the vector does not match the length of the array");
+                }
+                let _ = self.send(Box::new(PongMessage::new(nonce)));
+            }
+        }
+        // return the envelope parsed as a member of the right message class
+        let message_class = command_to_class.remove(&command).unwrap();
+        
+        return message_class
+    }
+}
+
+#[test]
+fn simple_node_test() {
+    let mut node = SimpleNode::new(
+        "testnet.programmingbitcoin.com",
+        None,
+        true,
+        true,
+    ).unwrap();
+
+    let version = VersionMessage::default();
+    
+    let _ = node.send(Box::new(version));
+    
+    let mut verack_received = false;
+    let mut version_received = false;
+
+    while !verack_received || !version_received {
+        let message = node.wait_for(
+            vec![Box::new(VersionMessage::default()), Box::new(VerAckMessage::new())]
+        );
+
+        if message.command() == vec![b'v', b'e', b'r', b'a', b'c', b'k'] {
+            verack_received = true;
+        } else {
+            version_received = true;
+        }
+    }
+    node.handshake();
 }
