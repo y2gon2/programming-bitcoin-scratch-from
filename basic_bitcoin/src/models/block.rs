f7cea6bed582c08f5a71e2e8503d73f82994a9e8
y2gon2 <y2gon2@gmail.com>
2023-08-04 19:40:18 +0900

    Added: fn validate_merkle_root

--- basic_bitcoin/src/models/block.rs
+++ basic_bitcoin/src/models/block.rs
@@ -35,6 +35,7 @@ pub struct Block {
     timestamp: u32,
     bits: [u8; 4],
     nonce: [u8; 4],
+    tx_hashes: Option<Vec<Vec<u8>>>,
 }
 
 impl Block {
@@ -45,6 +46,7 @@ impl Block {
         timestamp: u32,
         bits: [u8; 4],
         nonce: [u8; 4],
+        tx_hashes: Option<Vec<Vec<u8>>>,
     ) -> Self {
         Self { 
             version, 
@@ -53,6 +55,7 @@ impl Block {
             timestamp, 
             bits, 
             nonce, 
+            tx_hashes,
         }
     }
 
@@ -96,6 +99,7 @@ impl Block {
             timestamp,
             bits,
             nonce,
+            tx_hashes: None,
         }
     }
 
@@ -184,6 +188,25 @@ impl Block {
         let proof = BigUint::from_bytes_le(&h256);
 
         return proof < self.target() 
+    }
+
+
+    /// Gets the merkle root of the tx_hashes and checks 
+    /// that it's the same as the merkle root of this block.
+    pub fn validate_merkle_root(&self) -> bool {
+        let tx_hashes = self.tx_hashes
+            .as_ref()
+            .expect("Not exist tx_hashes in the Block");
+
+        let reverse_hashes: Vec<Vec<u8>> = tx_hashes
+            .iter()
+            .map(|hash| hash.iter().rev().cloned().collect())
+            .collect();
+        
+        let mut computed_root = merkle_root(reverse_hashes);
+        computed_root.reverse();
+    
+        return self.merkle_root.to_vec() == computed_root
     }
 
     pub fn get_timestamp(&self) -> u32 {
@@ -383,5 +406,33 @@ mod block_test {
         let mut block2 = Block::parse(&mut cursor2);
 
         assert!(!block2.check_pow());
+    }
+
+    #[test]
+    fn test_validate_merkle_root() {
+        let hashes = vec![
+            str_to_vec_u8("f54cb69e5dc1bd38ee6901e4ec2007a5030e14bdd60afb4d2f3428c88eea17c1"),
+            str_to_vec_u8("c57c2d678da0a7ee8cfa058f1cf49bfcb00ae21eda966640e312b464414731c1"),
+            str_to_vec_u8("b027077c94668a84a5d0e72ac0020bae3838cb7f9ee3fa4e81d1eecf6eda91f3"),
+            str_to_vec_u8("8131a1b8ec3a815b4800b43dff6c6963c75193c4190ec946b93245a9928a233d"),
+            str_to_vec_u8("ae7d63ffcb3ae2bc0681eca0df10dda3ca36dedb9dbf49e33c5fbe33262f0910"),
+            str_to_vec_u8("61a14b1bbdcdda8a22e61036839e8b110913832efd4b086948a6a64fd5b3377d"),
+            str_to_vec_u8("fc7051c8b536ac87344c5497595d5d2ffdaba471c73fae15fe9228547ea71881"),
+            str_to_vec_u8("77386a46e26f69b3cd435aa4faac932027f58d0b7252e62fb6c9c2489887f6df"),
+            str_to_vec_u8("59cbc055ccd26a2c4c4df2770382c7fea135c56d9e75d3f758ac465f74c025b8"),
+            str_to_vec_u8("7c2bf5687f19785a61be9f46e031ba041c7f93e2b7e9212799d84ba052395195"),
+            str_to_vec_u8("08598eebd94c18b0d59ac921e9ba99e2b8ab7d9fccde7d44f2bd4d5e2e726d2e"),
+            str_to_vec_u8("f0bb99ef46b029dd6f714e4b12a7d796258c48fee57324ebdc0bbc4700753ab1"),
+        ];
+
+        let stream = str_to_vec_u8("00000020fcb19f7895db08cadc9573e7915e3919f\
+            b76d59868a51d995201000000000000acbcab8bcc1af95d8d563b77d24c3d19b18f1486383d75a508\
+            5c4e86c86beed691cfa85916ca061a00000000");
+
+        let mut stream_cursor = Cursor::new(stream);
+        let mut block = Block::parse(&mut stream_cursor);
+        block.tx_hashes = Some(hashes);
+
+        assert!(block.validate_merkle_root());
     }
 }
